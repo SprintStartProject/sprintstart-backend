ba1903034588092cabd6492c4f1f8d35222d35bf
BrainSkript <a.johnsen02@gmail.com>
2026-09-11 19:08:16 +0200

    Fixed failed test

--- src/test/kotlin/com/sprintstart/sprintstartbackend/onboarding/service/OnboardingPathPersistenceTest.kt
+++ src/test/kotlin/com/sprintstart/sprintstartbackend/onboarding/service/OnboardingPathPersistenceTest.kt
@@ -13,10 +13,12 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest
 import org.springframework.context.annotation.Import
+import org.springframework.test.context.ActiveProfiles
 import java.util.UUID
 import kotlin.test.assertEquals
 
 @DataJpaTest
+@ActiveProfiles("test")
 @Import(CryptoConfiguration::class)
 class OnboardingPathPersistenceTest {
     @Autowired
